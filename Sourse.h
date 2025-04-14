67fd7ede3e398fb97e241125d934014f1df5c5d3
Maksim <141848155+SSAT00@users.noreply.github.com>
2025-04-14 09:53:39 +0700

    Delete Sourse.h

--- Sourse.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include<math.h>
-#include<iostream>
-
-using namespace std;
-extern "C" __declspec(dllexport) void Kepler2Pos(double*, double*);
-extern "C" __declspec(dllexport) void Pos2Kepler(double*, double*);
